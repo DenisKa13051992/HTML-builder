91a89225e445cbf893f5db45eb73060ca0e01e2e
Dzianis Karnachenka <arcadievich13051992@gmail.com>
2022-11-07 19:55:02 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir(){ fs.readdir(path.join(__dirname, 'files-copy'), (err, files)=>{
+    if (err) {
+        console.error(err)
+        return;
+      }
+    })
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, ()=>{fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      console.error(err)
+      return;
+    }
+  })
+fs.readdir(path.join(__dirname, 'files'), (err, files)=>{
+    if (err) {
+        console.error(err)
+        return;
+      }
+    files.forEach(file => {
+    fs.copyFile(`${path.join(__dirname, 'files')}/${file}`, `${path.join(__dirname, 'files-copy')}/${file}`, err => {    
+      if(err) throw err;    
+    });    
+    });
+})})
+}
+copyDir()
